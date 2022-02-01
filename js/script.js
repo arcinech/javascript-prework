2caad5ddd7a9eb10577148238053d5e41097ef20
Marcin Lech <arcinech@gmail.com>
2022-02-01 18:58:07 +0100

    Added player move

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-let computerMove = `nieznany ruch`;
-let playerMove = `nieznany ruch`;
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -11,8 +8,26 @@ if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumber == 2) {
     computerMove = 'papier';
-} else if (randomNumber == 3) {
+} else {
     computerMove = 'nożyce';
 }
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == 1) {
+    playerMove = 'kamień';
+} else if (playerInput == 2) {
+    playerMove = 'papier';
+} else if (playerInput == 3) {
+    playerMove = 'nożyce';
+} else {
+    printMessage('Nie zgrywaj cwaniaka. Przegrywasz walkowerem');
+}
+
+printMessage('Twój ruch to: ' + playerMove);
 
 printMessage('Mój ruch to: ' + computerMove);
