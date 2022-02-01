d07499b9265a83ce36e6b93d4ce5e7820ec60bf7
Marcin Lech <arcinech@gmail.com>
2022-02-01 20:40:31 +0100

    Added listeners for buttons

--- js/script.js
+++ js/script.js
@@ -53,4 +53,12 @@ function playGame(playerInput) {
 
 }
 
-playGame(3);
+document.getElementById('play-rock').addEventListener('click', function() {
+    playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function() {
+    playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function() {
+    playGame(3);
+});
