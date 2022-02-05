0a801774129f93f68a97b654e232451936236f39
arcinech <98415555+arcinech@users.noreply.github.com>
2022-02-05 16:45:30 +0100

    Fix: script.js wrong merging

--- js/script.js
+++ js/script.js
@@ -1,57 +1,3 @@
-function playGame(playerInput) {
-
-    clearMessages();
-
-    function getMoveName(argMoveId) {
-        if (argMoveId == 1) {
-            return 'kamień';
-        } else if (argMoveId == 2) {
-            return 'papier';
-        } else if (argMoveId == 3) {
-            return 'nożyce';
-        } else {
-            printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-            return 'nieznany ruch';
-        }
-    }
-
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    console.log('Wylosowana liczba to: 2') + randomNumber;
-
-    let computerMove = getMoveName(randomNumber);
-
-    console.log('Gracz wpisał: ' + playerInput);
-
-    let playerMove = getMoveName(playerInput);
-
-    function displayResult(argComputerMove, argPlayerMove) {
-        console.log('moves:', argComputerMove, argPlayerMove);
-        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-
-        if (argComputerMove == 'kamień' && playerMove == 'papier') {
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-            printMessage('Wygrywam!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-            printMessage('Wygrywam!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-            printMessage('Wygrywam!');
-        } else if (argComputerMove == argPlayerMove) {
-            printMessage('Remis!');
-        } else {
-            printMessage('Nie umiesz czytać?! Przegrywasz walkowerem.');
-        }
-    }
-
-    displayResult(computerMove, playerMove);
-}
-
-<<<<<<< HEAD
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -89,14 +35,3 @@ if (computerMove == 'kamień' && playerMove == 'papier') {
 } else {
     printMessage('Nie umiesz czytać?! Przegrywasz walkowerem.');
 }
-=======
-document.getElementById('play-rock').addEventListener('click', function() {
-    playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function() {
-    playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function() {
-    playGame(3);
-});
->>>>>>> 9183d78b10e4cf0af6564148804e8ff1d78fbbac
